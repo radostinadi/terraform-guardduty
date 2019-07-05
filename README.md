41e15849807044e3478624553e9fb8e1358fbba3
Yoveva <radostina.yoveva@accenture.com>
2019-07-05 16:20:13 +0300

    updating readme

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # terraform-guardduty
 
 Terraform module to enable the AWS GuardDuty service
+
+## Usage
+
+```hcl
+module "terraform-guardduty" {
+  source = "../../modules/terraform-guardduty"
+
+  sns_topic_name = <TOPIC NAME>
+}
+```
---
+++ cloudwatch.tf
@@ -0,0 +1,19 @@
+resource "aws_cloudwatch_event_rule" "guardduty-finding-events" {
+  name          = "guardduty-finding-events"
+  description   = "Capture AWS GuardDuty event findings"
+  event_pattern = "${file("${path.module}/guardduty-event-pattern.json")}"
+}
+
+resource "aws_cloudwatch_event_target" "sns" {
+  rule      = "${aws_cloudwatch_event_rule.guardduty-finding-events.name}"
+  target_id = "send-to-sns"
+  arn       = "${data.aws_sns_topic.guardduty.arn}"
+
+  input_transformer {
+    input_paths = {
+      title = "$.detail.title"
+    }
+
+    input_template = "\"GuardDuty finding: <title>\""
+  }
+}
