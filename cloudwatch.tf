f2bbe6d7bf1d5a4145ff89bef5ad7bbee64b3209
Yoveva <radostina.yoveva@accenture.com>
2019-07-05 17:49:39 +0300

    updating cloufwatch configiration

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -7,7 +7,7 @@ resource "aws_cloudwatch_event_rule" "guardduty-finding-events" {
 resource "aws_cloudwatch_event_target" "sns" {
   rule      = "${aws_cloudwatch_event_rule.guardduty-finding-events.name}"
   target_id = "send-to-sns"
-  arn       = "${data.aws_sns_topic.guardduty.arn}"
+  arn       = "${aws_sns_topic.guardduty_sns_topic.arn}"
 
   input_transformer {
     input_paths = {
